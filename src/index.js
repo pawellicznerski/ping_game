6f93eeb8e2944d98acc6841f848ad6c22536357b
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-04-04 22:01:06 +0200

    attempt on adding moving ball function

--- src/index.js
+++ src/index.js
@@ -11,12 +11,32 @@ const ball = new Ball();
 
 const leftPallet = document.getElementsByClassName('pallet_left')[0];
 document.addEventListener("mousemove",(e)=>{
-    console.log("clientY ",e.clientY);
-    console.log("window.innerHeight ",window.innerHeight);
+    // console.log("clientY ",e.clientY);
+    // console.log("window.innerHeight ",window.innerHeight);
     const topPosition = window.innerHeight<=e.clientY+100?window.innerHeight-100:e.clientY;
     leftPallet.setAttribute("style",`top:${topPosition}px`)
 });
 
-// function myFunction(){
-//   console.log("dupa");
-// }
+const startBtn = document.getElementsByClassName('start-btn')[0];
+startBtn.addEventListener('click',moveBall);
+
+function moveBall(){
+  let currentLeft = 180;
+  // const currentvalue = rondomValue<=0.5?-1:1;
+  let changeVertical = 1;
+
+  const id = setInterval(movingBall,10,currentLeft,changeVertical);
+}
+
+function movingBall(currentLeft,changeVertical){
+  console.log(currentLeft);
+  const theBall = document.getElementById('ball');
+  if(theBall.left<=0||theBall.left+20>=window.innerWidth){
+    changeVertical=changeVertical*(-1);
+  }
+
+  const rondomValue = Math.random();
+  theBall.setAttribute("style",`left:${currentLeft}`);
+  currentLeft=currentLeft+changeVertical;
+  console.log(currentLeft);
+}

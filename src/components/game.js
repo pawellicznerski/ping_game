1351bb355f655d1f34e8dd229e3c769ab0d30f5d
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-04-06 09:23:54 +0200

    improving computer pallet

--- src/components/game.js
+++ src/components/game.js
@@ -20,35 +20,30 @@ export default class Game{
     document.addEventListener("mousemove",(e)=>{
       const topPosition = window.innerHeight<=e.clientY+100?window.innerHeight-100:e.clientY;
       palletPlayer.palletDOM.setAttribute("style",`top:${topPosition}px`)
-      // const palletDOM = document.getElementsByClassName('pallet_left');
-      // console.log("top",document.getElementsByClassName('pallet_left')[0].offsetTop);
-      // console.log('palletTop',palletTop);
-
     });
 
     const id = setInterval(movingBall,1);
 
     function movingBall(){
-      const palletTop= document.getElementsByClassName('pallet_left')[0].offsetTop;
-      // let ballDirectionTopVal=1;
-      // console.log("currentLeft",currentLeft,ball.ballDirectionLeft);
-      // const theBall = document.getElementById('ball');
-      // console.log("theBall",theBall);
-      // console.log('palletTop',palletTop);
+      const palletTopLeft= document.getElementsByClassName('pallet_left')[0].offsetTop;
+      const palletTopRight= document.getElementsByClassName('pallet_right')[0].offsetTop;
 
-      // if(ball.ballLeft<=20||ball.ballLeft+40>=window.innerWidth){
-      if(ball.ballLeft<=20){
-        if(palletTop<=ball.ballTop+20&&palletTop+100>=ball.ballTop-20){
+      if(ball.ballLeft==20){
+        if(palletTopLeft<=ball.ballTop+20&&palletTopLeft+100>=ball.ballTop-20){
           ball.ballDirectionLeft=ball.ballDirectionLeft*(-1);
           ball.ballDirectionTop=Math.random()>0.5?-1:1;
           ball.ballSpeed=(((Math.random()*2.9)+0.1)*window.innerWidth)/window.innerWidth;
-          // console.log('ball.ballSpeed',ball.ballSpeed);
+          console.log(ball.ballSpeed);
+        }
+      } else if(ball.ballLeft+40==window.innerWidth){
+        if(palletTopRight<=ball.ballTop+20&&palletTopRight+100>=ball.ballTop-20){
+          ball.ballDirectionLeft=ball.ballDirectionLeft*(-1);
+          ball.ballDirectionTop=Math.random()>0.5?-1:1;
+          ball.ballSpeed=(((Math.random()*2.9)+0.1)*window.innerWidth)/window.innerWidth;
         }
       }
-      if(ball.ballLeft+20>=window.innerWidth){
-        ball.ballDirectionLeft=ball.ballDirectionLeft*(-1);
-      }
-      if(ball.ballLeft<=0){
+
+      if(ball.ballLeft<=0||ball.ballLeft+20>=window.innerWidth){
         info.add();
         ball.setInitialState();
         clearInterval(id);
@@ -58,12 +53,14 @@ export default class Game{
         ball.ballDirectionTop=ball.ballDirectionTop*(-1);
       }
 
-      if(ball.ballLeft<=window.innerWidth&&ball.ballDirectionLeft>0){
-        if(ball.ballTop+20<palletComp.palletTop){
-
-        }
+      if(ball.ballTop<palletComp.palletTop){
+        palletComp.palletTop=palletComp.palletTop-2;
+        palletComp.palletDOM.setAttribute("style",`top:${palletComp.palletTop}px`)
+      } else if(ball.ballTop>palletComp.palletTop+91){
+        palletComp.palletTop=palletComp.palletTop+2;
         palletComp.palletDOM.setAttribute("style",`top:${palletComp.palletTop}px`)
       }
+
 
       const rondomValue = Math.random();
       ball.ballDOM.setAttribute("style",`top:${ball.ballTop}px;left:${ball.ballLeft}px`);
